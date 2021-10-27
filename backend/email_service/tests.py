c6dc6f606f303b15349a41915721f990a5df1b66
George Johnson <georgemjohnson11@gmail.com>
2021-10-26 19:31:21 -0600

    Add and update tests for future fixes

--- backend/email_service/tests.py
+++ backend/email_service/tests.py
@@ -1,3 +1,20 @@
+from django.core import mail
 from django.test import TestCase
+from rest_framework.test import RequestsClient
 
-# Create your tests here.
+class EndpointTest(TestCase):
+    def test_github_request(self):
+        client = RequestsClient()
+        response = client.get('http://testserver/api/georgemjohnson11/auth_system/')
+        assert response.status_code == 200
+
+#https://docs.djangoproject.com/en/3.2/topics/testing/tools/#email-services
+class EmailTest(TestCase):
+    def test_send_email(self):
+        mail.send_mail(
+            'Subject here', 'Here is the message.',
+            'from@example.com', ['to@example.com'],
+            fail_silently=False,
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
